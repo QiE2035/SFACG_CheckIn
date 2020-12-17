d9073c40c7d6ea4aee58df95a345c66215171a25
企鹅2035 <qie2035@qq.com>
2020-12-18 06:43:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import urllib2,os
-try:
-    sfacg_data = os.environ["sfacg_data"]
-    cookie = os.environ["cookie"]
-    auth = os.environ["auth_data"]
-    requrl = "https://api.sfacg.com/user/signInfo"
-    headerdata = {"Cookie":cookie,"SFSecurity":sfacg_data,"Authorization":auth,"User-Agent":"boluobao/4.6.36(android;22)/OPPO"}
-    request = urllib2.Request(requrl,None,headerdata)
-    request.get_method = lambda: 'PUT'
-    response = urllib2.urlopen(request)
-    print response.read()
-except BaseException,Argument:
-    print "error:",Argument
+sfacg_data = os.environ["sfacg_data"]
+cookie = os.environ["cookie"]
+auth = os.environ["auth_data"]
+requrl = "https://api.sfacg.com/user/signInfo"
+headerdata = {"Cookie":cookie,"SFSecurity":sfacg_data,"Authorization":auth,"User-Agent":"boluobao/4.6.36(android;22)/OPPO"}
+request = urllib2.Request(requrl,None,headerdata)
+request.get_method = lambda: 'PUT'
+response = urllib2.urlopen(request)
+print response.read()
